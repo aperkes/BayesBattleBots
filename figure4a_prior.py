6b68c6e15b7049e22f7ce7afd9900c0cb7c7e049
aperkes <perkes.ammon@gmail.com>
2023-03-02 15:19:11 -0800

    added multiple trials and getting closer to real figure

--- figure4a_prior.py
+++ figure4a_prior.py
@@ -10,6 +10,7 @@ from params import Params
 
 import numpy as np
 from scipy.stats import binom_test,norm
+from scipy.stats import f_oneway
 
 from matplotlib import pyplot as plt
 from matplotlib import cm
@@ -29,14 +30,15 @@ if True:
 params.poly_param_a = 2
 params.poly_param_c = 0.1
 params.n_fights = 50 
-params.size = 50
+#params.size = 50
 params.energy_cost = False
 params.acuity = 1
 params.awareness = 5
 params.start_energy = 1
 
-params.iterations = 10000
+params.iterations = 100
 params.n_fish = 5
+params.n_fights = 3
 params.f_method = 'balanced'
 params.u_method = 'bayes'
 params.f_outcome = 'math'
@@ -55,7 +57,7 @@ win_shifts,loss_shifts = [],[]
 ## Lets assume their estimate is correct.
 
 params.age = 52
-params.size = 50 
+#params.size = 50 
 
 ## Set up the various fish conditions
 #size_params = params.copy() ## Fish determine prior from their size
@@ -92,10 +94,11 @@ for n in tqdm(range(params.iterations)):
     f.prior = prior / np.sum(prior)
     f.get_stats()
     guess_fishes.append(f)
+    null_fishes[n].get_stats()
 
 for n in tqdm(range(params.iterations)):
     f = Fish(n+1,guess_params)
-
+    #print(null_fishes[n].estimate,null_fishes[n].size)
 opp_fish = Fish(0,opp_params)
 
 #print(opp_fish.estimate)
@@ -103,41 +106,77 @@ opp_fish = Fish(0,opp_params)
 p_efforts,n_efforts,g_efforts = [],[],[]
 b_efforts,s_efforts = [],[]
 
+effort_array = np.empty([params.iterations,5,3])
+
+
 for n in tqdm(range(params.iterations)):
-    self_fight = Fight(self_fishes[n],opp_fish)
-    self_fight.run_outcome()
-
-    null_fight = Fight(null_fishes[n],opp_fish)
-    null_fight.run_outcome()
-
-    guess_fight = Fight(guess_fishes[n],opp_fish)
-    guess_fight.run_outcome()
-
-    big_fight = Fight(big_fishes[n],opp_fish)
-    big_fight.run_outcome()
-
-    small_fight = Fight(small_fishes[n],opp_fish)
-    small_fight.run_outcome()
     p,o = self_fishes[n],null_fishes[n]
     g = guess_fishes[n]
     b,s = big_fishes[n],small_fishes[n]
 
+    self_fight = Fight(self_fishes[n],opp_fish)
+    null_fight = Fight(null_fishes[n],opp_fish)
+    guess_fight = Fight(guess_fishes[n],opp_fish)
+    big_fight = Fight(big_fishes[n],opp_fish)
+    small_fight = Fight(small_fishes[n],opp_fish)
+
+    for m in range(params.n_fights):
+        self_fight.run_outcome()
+        null_fight.run_outcome()
+        guess_fight.run_outcome()
+        big_fight.run_outcome()
+        small_fight.run_outcome()
+
+        effort_array[n,0,m] = p.effort
+        effort_array[n,1,m] = o.effort
+        effort_array[n,2,m] = g.effort
+        effort_array[n,3,m] = b.effort
+        effort_array[n,4,m] = s.effort
     p_efforts.append(p.effort)
     n_efforts.append(o.effort)
     g_efforts.append(g.effort)
     b_efforts.append(b.effort)
     s_efforts.append(s.effort)
-
+    print(o.size,o.effort)
 p_mean,p_std = np.mean(p_efforts),np.std(p_efforts)
 n_mean,n_std = np.mean(n_efforts),np.std(n_efforts)
 g_mean,g_std = np.mean(g_efforts),np.std(g_efforts)
 print(np.mean(p_efforts),np.mean(n_efforts),np.mean(g_efforts))
 print(np.std(p_efforts),np.std(n_efforts),np.std(g_efforts))
 
+print('big mean:',np.mean(effort_array[:,0,:],axis=0))
+
 #fig,(ax,ax2,ax3) = plt.subplots(3)
+print('SELF ONE WAY ANOVA:')
+print(f_oneway(*effort_array[:,0]))
+
+print('Uniform ONE WAY ANOVA:')
+print(f_oneway(*effort_array[:,1]))
+
 fig,ax = plt.subplots()
 #ax.bar([1,2,3],[p_mean,n_mean,g_mean],yerr=[p_std,n_std,g_std],alpha=.1)
-ax.boxplot([n_efforts,g_efforts,p_efforts,s_efforts,b_efforts])
+p_norms = np.mean(effort_array[:,0],axis=1)
+n_norms = np.mean(effort_array[:,1],axis=1)
+g_norms = np.mean(effort_array[:,2],axis=1)
+b_norms = np.mean(effort_array[:,3],axis=1)
+s_norms = np.mean(effort_array[:,4],axis=1)
+
+p_sizes = [self_fishes[f].size/100+2.5 for f in range(params.iterations)]
+n_sizes = [null_fishes[f].size/100+0.5 for f in range(params.iterations)]
+g_sizes = [guess_fishes[f].size/100+1.5 for f in range(params.iterations)]
+b_sizes = [big_fishes[f].size/100+4.5 for f in range(params.iterations)]
+s_sizes = [small_fishes[f].size/100+3.5 for f in range(params.iterations)]
+
+ax.boxplot([n_norms,g_norms,p_norms,s_norms,b_norms],widths=0.1)
+
+ax.scatter(p_sizes,p_norms,alpha=0.3)
+ax.scatter(n_sizes,n_norms,alpha=0.3)
+ax.scatter(g_sizes,g_norms,alpha=0.3)
+ax.scatter(b_sizes,b_norms,alpha=0.3)
+ax.scatter(s_sizes,s_norms,alpha=0.3)
+
+#ax.scatter([self_fishes[f].size/100+0.5 for f in range(params.iterations)],np.mean(effort_array[:,1],axis=1))
+
 ax.set_xticklabels(['Uniform\nPrior','Random\nPrior','Self-Informed\nPrior','Small-Informed\nPrior','Big-Informed\nPrior'],rotation=45)
 ax.axvline(2.5,linestyle=':',color='black')
 #ax2.scatter([f.guess for f in prior_fishes],[f.effort for f in prior_fishes])
