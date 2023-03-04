d8c7f1c829fb211c9930efe10ce67c2bb72406c2
Ammon Perkes <perkes.ammon@gmail.com>
2023-03-03 17:10:49 -0800

    figure works, loks good, needs axes, parameters

--- figure2_intensity.py
+++ figure2_intensity.py
@@ -9,6 +9,7 @@ For questions, contact Ammon Perkes (perkes.ammon@gmail.com)
 import numpy as np
 from matplotlib import pyplot as plt
 import copy
+from tqdm import tqdm
 
 from fish import Fish
 from fight import Fight
@@ -24,8 +25,8 @@ SAVE = False
 params = Params()
 params.outcome_params = [0.6,0.3,.1]
 params.set_L()
-params.size=50
-params.prior = True
+#params.size=50
+#params.prior = True
 
 #params.effort_method = [None,'!']
 #params.effort_method = 'ExplorePoly'
@@ -36,45 +37,29 @@ params.awareness = 15
 params.acuity = 2
 params.post_acuity = True
 params.f_method = 'shuffled'
-params.n_fights = 30
 
+params.n_fish = 5
+params.n_fights = 30
 params.iterations = 100
+params.n_rounds = params.n_fights * (params.n_fish-1)
 
 ## Set up a tank
-for i in range(params.iterations):
+cost_array = np.empty([params.iterations,params.n_fish,params.n_rounds])
+for i in tqdm(range(params.iterations)):
     fishes = [Fish(f,params) for f in range(5)]
 
     tank = Tank(fishes,params)
-    tank.run_all()
+    tank.run_all(progress=False)
 
-#effort_array = np.zeros([len(fishes),len(win_record)])
-cost_array = np.zeros([len(fishes),len(win_record)])
-for f in range(len(fishes)):
-    cost_record = np.array(tank.fishes[f].win_record)[:,3]
-    #effort_record = np.array(tank.fishes[f].win_record)[:,2]
-    #effort_array[f] = effort_record
-    cost_array[f] = cost_record
+    for f in range(len(fishes)):
+        cost_array[i,f] = np.array(tank.fishes[f].win_record)[:,3]
 
-fig2,ax2 = plt.subplots()
 
-smooth_costs = gaussian_filter1d(np.mean(cost_array,0),3)
-smooth_effort = gaussian_filter1d(np.mean(effort_array,0),3)
-ax2.plot(smooth_costs*4,color='black')
-ax2.plot(smooth_effort*4,linestyle=':',color='black')
-
-## Figure 3c: Estimate over time, to show that bayes is accurate
-
-fig3,ax3 = tank.plot_estimates(food=False)
-
-fig4,ax4 = plt.subplots()
-f_estimates = [f.estimate for f in fishes]
-f_max = np.argmax(f_estimates)
-f = f_max
-print(f)
-ax4.plot(fishes[f].xs,fishes[f].prior)
-ax4.axvline(fishes[f].size,color='black')
-ax4.axvline(fishes[f].range_record[-1][1])
-ax4.axvline(fishes[f].range_record[-1][2])
+fig,ax = plt.subplots()
+mean_int = np.nanmean(cost_array,axis=(0,1))
+sem_int = np.std(cost_array,axis=(0,1)) / np.sqrt(params.iterations)
+ax.plot(mean_int,color='black')
+ax.fill_between(range(params.n_rounds),mean_int-sem_int,mean_int+sem_int,color='gray',alpha=0.5)
 
 if PLOT:
     plt.show()
