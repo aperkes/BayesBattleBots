d24c0a569eaedfb08282099ef48f6ef21bcbb405
aperkes <perkes.ammon@gmail.com>
2023-04-04 15:51:06 -0700

    ternary plot works finally!

--- figureXX_fight_sfl.py
+++ figureXX_fight_sfl.py
@@ -3,21 +3,32 @@
 ## Script to iteration through the s,f,l, parameter space 
 ## showing who would win in a fight between two fish
 
+import numpy as np
+from tqdm import tqdm
+
 from params import Params
 from fish import Fish
 from fight import Fight
 
 ## there's a transform here, so for the exponentional
-## -1 => 0
+## -1 => infinity 
 ## 0 => 1
-## 1 == infinity  
+## 1 == 0  
 
-fight_params = Params()
-fight_params.outcome_params = [1,0,-1]
-fight_params.set_params()
+def build_params(s,f,l):
+    fight_params = Params()
+    fight_params.outcome_params = [s,f,l]
+    fight_params.set_params()
+    return fight_params
 
-f1_params = fight_params.copy()
-f2_params = fight_params.copy()
+iterations=2
+scale=21
+p_space = np.linspace(-1,1,scale)
+p_array = np.zeros([scale,scale,scale])
+s,f,l = [0,0,0]
+
+f1_params = Params()
+f2_params = f1_params.copy()
 
 f1_params.size = 60
 f1_params.effort_method = [None,0.3]
@@ -27,13 +38,29 @@ f2_params.size = 50
 f2_params.effort_method = [None,0.6]
 f2_params.baseline_effort = 0.6
 
-f1 = Fish(1,f1_params)
-f2 = Fish(2,f2_params)
+f1 = Fish(0,f1_params)
+f2 = Fish(1,f2_params)
 
-fight = Fight(f1,f2,fight_params)
-outcome = fight.run_outcome()
 
-print(outcome,f1.effort,f2.effort)
-print(fight.winner.idx)
-print(fight.p_win)
-print(f1.params.L,f1.params.F,f1.params.S)
+for s_ in tqdm(range(scale)):
+    s = p_space[s_]
+    for f_ in range(scale):
+        f = p_space[f_]
+        for l_ in range(scale):
+            l = p_space[-(1 + l_)]
+
+            fight_params = build_params(s,f,l)
+
+            fight = Fight(f1,f2,fight_params)
+            outcome = fight.run_outcome()
+            #import pdb;pdb.set_trace()
+            if fight.f_min == 1:
+                p_array[s_,f_,l_] = fight.p_win
+            else:
+                p_array[s_,f_,l_] = 1 - fight.p_win
+
+
+print(np.round(p_array[:,0,:],3))
+np.save('./results/fight_slf.npy',p_array)
+
+print('done!')
--- make_ternary.py
+++ make_ternary.py
@@ -33,25 +33,42 @@ def np_to_dict(a,p_list,keys=['s','f','l']):
                 p_dict[key] = a[s_,f_,l_]
     return p_dict
 
-def plot_from_dict(p_dict):
-    fig,tax = ternary.figure(scale=10)
+def plot_from_dict(p_dict,scale=10):
+    fig,tax = ternary.figure(scale=scale)
     tax.heatmap(p_dict,cmap=None)
     return fig,tax 
 
 if __name__ == '__main__':
-    a = np.load('./results/lin_tern_0.npy')
+    #a = np.load('./results/lin_tern_0.npy')
+    a = np.load('./results/fight_slf.npy')
+    #a = np.mean(a,axis=3)
+    a_dim = a.shape[0]
+    a_buffer = np.pad(a,1,mode='edge')
+    scale = a_dim-1
+    p_list = np.linspace(-1,1,scale)
+    if True:
+        a = a_buffer
+        scale = scale+2
+        p_list = np.pad(p_list,1,mode='edge')
     if False: ## Helpful for visualization
         a[:,2,:] = 0
         a[:,3,:] = 0
         a[4,:,:] = 0
         a[5,:,:] = 0
-    p_list = [-1,-0.9,-0.8,-0.2,-0.1,0,0.1,0.2,0.8,0.9,1]
+    #p_list = [-1,-0.9,-0.8,-0.2,-0.1,0,0.1,0.2,0.8,0.9,1]
     p_dict = np_to_dict(a,p_list)
-    fig,tax = plot_from_dict(p_dict)
+    fig,tax = plot_from_dict(p_dict,scale)
     tax.bottom_axis_label("s")
     tax.right_axis_label("f")
     tax.left_axis_label("l")
     tax.ticks(axis='brl')
+    if False:
+        tax._ticks['b'] = list(p_list.astype(str))
+        tax._ticks['r'] = list(p_list.astype(str))
+        tax._ticks['l'] = list(p_list.astype(str))
+        tax.set_custom_ticks()
+    tax.clear_matplotlib_ticks()
+    tax.set_title('Probability of winning over s,f,l')
     tax.show()
 
 
