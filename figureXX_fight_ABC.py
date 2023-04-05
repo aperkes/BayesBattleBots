6a1d55edd469d8810cffd0071cfc0a2712991abc
aperkes <perkes.ammon@gmail.com>
2023-04-05 15:06:50 -0700

    new script to explore effort params

--- figureXX_fight_ABC.py
+++ figureXX_fight_ABC.py
@@ -2,18 +2,42 @@
 
 # Script to explore effort parameters
 
+import numpy as np
+
 from fish import Fish
 from fight import Fight
 from params import Params
 
-params = Params(size=50)
+from tqdm import tqdm
 
-f1 = Fish(1,params)
-f2 = Fish(2,params)
-fight = Fight(f1,f2,params)
+f1_params = Params(size=50)
+f2_params = Params(size=40)
 
-fight.run_outcome()
-#eff = f1.choose_effort(f2,fight)
-print(f1.size,f2.size)
-print(f1.guess,f1.estimate,f2.guess,f2.estimate)
-print(f1.effort)
+iterations = 1000
+
+scale = 11
+ac_scale = np.linspace(0,1,scale)
+b_scale = np.linspace(-1,1,scale)
+
+eff_array = np.zeros([scale,scale,scale,iterations])
+
+for a_ in tqdm(range(scale)):
+    a = ac_scale[a_]
+    for b_ in range(scale):
+        b = b_scale[b_]
+        for c_ in range(scale):
+            c = ac_scale[c_]
+            f1_params.awareness = a
+            f1_params.boldness = b
+            f1_params.acuity = c
+            f1_params.set_params()
+            for i in range(iterations):
+                f1 = Fish(1,f1_params)
+                f2 = Fish(2,f2_params)
+                fight = Fight(f1,f2,f1_params)
+                fight.run_outcome()
+                eff_array[a_,b_,c_,i] = f1.effort
+
+print(np.mean(eff_array[5,5,5]),np.std(eff_array[5,5,5]))
+
+np.save('./results/eff_array.npy',eff_array)
