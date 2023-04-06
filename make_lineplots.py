2ece893e962b1160e212325c90f77985490dd8a6
aperkes <perkes.ammon@gmail.com>
2023-04-05 22:22:28 -0700

    cleaned up plots and added axis labels

--- make_lineplots.py
+++ make_lineplots.py
@@ -14,25 +14,27 @@ med_a = a[5]
 high_a =a[3]
 
 mean_low = np.mean(low_a,axis=1)
-std_low = np.std(low_a,axis=1) / np.sqrt(1000)
+std_low = np.std(low_a,axis=1) / 10 
 
 mean_med = np.mean(med_a,axis=1)
-std_med = np.std(med_a,axis=1) / np.sqrt(1000)
+std_med = np.std(med_a,axis=1) / 10
 
 mean_high = np.mean(high_a,axis=1)
-std_high = np.std(high_a,axis=1) / np.sqrt(1000)
+std_high = np.std(high_a,axis=1) / 10
 
 fig,ax  = plt.subplots()
 
 xs = np.linspace(0,1,11)
 ax.plot(xs,mean_low,color='black')
-ax.fill_between(xs,mean_low - std_low,mean_low + std_low,alpha=0.5,color='red')
+ax.fill_between(xs,mean_low - std_low,mean_low + std_low,alpha=0.5,color='red',label='a = 0.3')
 
 ax.plot(xs,mean_med,color='black')
-ax.fill_between(xs,mean_med - std_med,mean_med + std_med,alpha=0.5,color='blue')
+ax.fill_between(xs,mean_med - std_med,mean_med + std_med,alpha=0.5,color='blue',label='a = 0.5')
 
 ax.plot(xs,mean_high,color='black')
-ax.fill_between(xs,mean_high - std_high,mean_high + std_high,alpha=0.5,color='green')
+ax.fill_between(xs,mean_high - std_high,mean_high + std_high,alpha=0.5,color='green',label='a = 0.7')
 
+ax.set_xlabel('Effort')
+ax.set_ylabel('Acuity')
 fig.savefig('./figures/line_plotAC.png',dpi=300)
 
