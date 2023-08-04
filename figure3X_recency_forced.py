76e0c6b9d92a0a77a2d0d3f0d7ebbc1f1e9e6154
aperkes <perkes.ammon@gmail.com>
2023-08-03 17:22:16 -0700

    made a fancy interactive demo

--- figure3X_recency_forced.py
+++ figure3X_recency_forced.py
@@ -49,6 +49,10 @@ params.size = 50
 def build_fish(idx,f0):
     return Fish(idx,f0.params)
 
+## There's a copy method, but it drops the history and estimate
+def clone_fish(f0):
+    return copy.deepcopy(f0)
+
 def check_success(f,f_match,params):
     fight = Fight(f,f_match,params)
     fight.run_outcome()
@@ -66,10 +70,12 @@ def build_results(n_matches=1):
 ## Dict to convert from outcome to letters
 conversion_dict = {0:'w',1:'l'}
 
-s_set = [-1,-0.3,0,0.3,1]
+#s_set = [-1,-0.3,0,0.3,1]
+s_set = list(np.linspace(-1,1,11))
 e_set = list(s_set)
 l_set = list(s_set)
-a_set = [0,0.5,1]
+#a_set = [0,0.5,1]
+a_set = list(np.linspace(0,1,11))
 c_set = list(a_set)
 n_C = len(s_set) ## as in density of dimensionality
 n_A = len(a_set)
@@ -79,19 +85,27 @@ wl_array = np.empty_like(decay_array)
 rec_array = np.empty_like(decay_array)
 late_array = np.empty_like(decay_array)
 
+est_array = np.empty_like(decay_array,dtype=dict)
+prob_array = np.empty_like(decay_array,dtype=dict)
+
 c_indices = range(n_C)
 a_indices = range(n_A)
 base_results = build_results(params.n_rounds)
 n_results = [[] for m in range(params.n_rounds)]
+"""
+NOTE: something is weird here, 
+The p_win is always equal to 0.5, but should obviously change
+need to figure out why, but then it works really well.
+"""
 for k in base_results.keys():
     for m in range(params.n_rounds):
         if len(k) == m:
             n_results[m].append(k)
 
+print('Working on it...better just find something else to work on for now')
 for (s_,e_,l_,a_,c_) in itertools.product(c_indices,c_indices,c_indices,a_indices,a_indices):
     s,e,l = s_set[s_],e_set[e_],l_set[l_]
     a,c = a_set[a_],c_set[c_]
-    print(s,e,l,a,c)
 
     params.awareness = a
     params.acuity = c
@@ -103,6 +117,7 @@ for (s_,e_,l_,a_,c_) in itertools.product(c_indices,c_indices,c_indices,a_indice
     previous_round = ['']
     match_results = dict(base_results)
     match_prob = {}
+    match_est = {}
 ## NOTE: I only actually need as many iterations as 
 # there are permutations, if I were smarter about this
 #for i in tqdm(range(iterations)):
@@ -116,7 +131,10 @@ for (s_,e_,l_,a_,c_) in itertools.product(c_indices,c_indices,c_indices,a_indice
         for exp in previous_round:
             f = fishes[exp]
             for w in [0,1]: ## force one of two conditions
-                f_ = build_fish(1,f)
+                #import pdb;pdb.set_trace()
+                #f_ = build_fish(1,f)
+                #f_ = f.copy()
+                f_ = copy.deepcopy(f)
                 match,_ = check_success(f_,f_match,params)
                 results_str = exp + conversion_dict[w]
                 f_.update(1-w,match) 
@@ -126,11 +144,13 @@ for (s_,e_,l_,a_,c_) in itertools.product(c_indices,c_indices,c_indices,a_indice
             else:
                 p_win = match.p_win
             match_prob[exp] = p_win
+            match_est[exp] = fishes[exp].estimate
             previous_round = this_round
     wl_effect = match_prob['w'] - match_prob['l']
     rec_effect = match_prob['lw'] - match_prob['wl']
     rec_effect_late = match_prob['llw'] - match_prob['wwl']
     rec_decay = np.abs(rec_effect_late) * np.sign(rec_effect) * np.sign(rec_effect_late)
+    #print(s,e,l,a,c,wl_effect)
 
 ## This happens when a certain event never occurs
     #if np.isnan(rec_decay):
@@ -139,5 +159,15 @@ for (s_,e_,l_,a_,c_) in itertools.product(c_indices,c_indices,c_indices,a_indice
     wl_array[s_,e_,l_,a_,c_] = wl_effect
     rec_array[s_,e_,l_,a_,c_] = rec_effect
     late_array[s_,e_,l_,a_,c_] = rec_effect_late
+    est_array[s_,e_,l_,a_,c_] = match_est
+    prob_array[s_,e_,l_,a_,c_] = match_prob
+
+## What do you do with info? I guess save it: 
+np.save('decay_array_dense.npy',decay_array)
+np.save('wl_array_dense.npy',wl_array)
+np.save('rec_array_dense.npy',rec_array)
+np.save('late_array_dense.npy',late_array)
+np.save('est_array_dense.npy',est_array)
+np.save('prob_array_dense.npy',prob_array)
 
 import pdb;pdb.set_trace()
---
+++ plot_chaos.py
@@ -0,0 +1,116 @@
+#! /usr/bin/env python
+
+
+## Needs to take a dict of estimates or probabilities and make the chaos figure
+
+from matplotlib import pyplot as plt
+import numpy as np
+from matplotlib.widgets import Slider, Button, RadioButtons
+
+
+est_array = np.load('./est_array.npy',allow_pickle=True)
+prob_array = np.load('./prob_array.npy',allow_pickle=True)
+
+est_dict = est_array[2,2,2,1,1]
+prob_dict = prob_array[2,2,2,1,1]
+
+MAX_THIC = 5
+params = [2,2,2,1,1]
+
+def plot_dict(est_dict,prob_dict = None):
+    #fig,ax = plt.subplots()
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    fig.subplots_adjust(left=0.25,bottom=0.45)
+
+    line_dict = {}
+    for k in est_dict.keys():
+        for k2 in est_dict.keys():
+            if len(k2) == len(k) + 1 and k2[:-1] == k:
+                if k2[0] == 'l':
+                    cor = 'darkblue'
+                else:
+                    cor = 'gold'
+                if prob_dict is not None:
+                    thic = prob_dict[k]
+                    if k2[-1] == 'l':
+                        thic = 1-thic
+                    thic = thic * MAX_THIC
+                    thic = np.clip(thic,1,MAX_THIC)
+                else:
+                    thic = None
+                [line_dict[k2]] = ax.plot([len(k),len(k2)],[est_dict[k],est_dict[k2]],linewidth=thic,color=cor,alpha=0.5)
+
+    return fig,ax,line_dict
+
+def sliders_on_changed(val):
+    #s,f,l,a,c = params
+    s = s_slider.val
+    f = f_slider.val
+    l = l_slider.val
+    a = a_slider.val
+    c = c_slider.val
+
+    est_dict = est_array[s,f,l,a,c]
+    prob_dict = prob_array[s,f,l,a,c]
+    update_lines(est_dict,prob_dict)
+    fig.canvas.draw_idle()
+
+def update_lines(est_dict,prob_dict):
+    for k in est_dict.keys():
+        t0 = k
+        for k2 in est_dict.keys():
+            if len(k2) == len(k) + 1 and k2[:-1] == k:
+                thic = prob_dict[k]
+                if k2[-1] == 'l':
+                    thic = 1-thic
+                thic = thic * MAX_THIC
+                thic = np.clip(thic,1,MAX_THIC)
+
+                #thic = prob_dict[k]
+                xs = [len(k),len(k2)]
+                ys = [est_dict[k],est_dict[k2]]
+                line_dict[k2].set_data(xs,ys)
+                line_dict[k2].set_linewidth(thic)
+    #print(k,k2)
+    return 0 ## I know this doesn't do anything, but I feel weird without an ending
+
+fig,ax,line_dict = plot_dict(est_dict,prob_dict)
+
+axis_color = 'lightgoldenrodyellow'
+
+## Add 5 slider bars
+s_slider_ax = fig.add_axes([0.25,0.3,0.65,0.03], facecolor=axis_color)
+s_slider = Slider(s_slider_ax,'s',0,4,valstep=1,valinit=2)
+s_slider.on_changed(sliders_on_changed)
+
+f_slider_ax = fig.add_axes([0.25,0.25,0.65,0.03], facecolor=axis_color)
+f_slider = Slider(f_slider_ax,'f',0,4,valstep=1,valinit=2)
+f_slider.on_changed(sliders_on_changed)
+
+l_slider_ax = fig.add_axes([0.25,0.20,0.65,0.03], facecolor=axis_color)
+l_slider = Slider(l_slider_ax,'l',0,4,valstep=1,valinit=2)
+l_slider.on_changed(sliders_on_changed)
+
+a_slider_ax = fig.add_axes([0.25,0.15,0.65,0.03], facecolor=axis_color)
+a_slider = Slider(a_slider_ax,'sigma_a',0,2,valstep=1,valinit=1)
+a_slider.on_changed(sliders_on_changed)
+
+c_slider_ax = fig.add_axes([0.25,0.1,0.65,0.03], facecolor=axis_color)
+c_slider = Slider(c_slider_ax,'sigma_c',0,2,valstep=1,valinit=1)
+c_slider.on_changed(sliders_on_changed)
+
+## add a reset button
+reset_button_ax = fig.add_axes([0.8,0.025,0.1,0.04])
+reset_button = Button(reset_button_ax, 'Reset', color=axis_color,hovercolor='0.975')
+def reset_button_on_clicked(mouse_event):
+    s_slider.reset()
+    f_slider.reset()
+    l_slider.reset()
+    a_slider.reset()
+    c_slider.reset()
+reset_button.on_clicked(reset_button_on_clicked)
+
+plt.show()
+
+
