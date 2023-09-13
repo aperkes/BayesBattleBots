8dbb785cd089dd3cbebd8f91fe81223a75ba3ff9
Ammon Perkes <perkes.ammon@gmail.com>
2023-09-13 16:07:44 -0700

    One parameter exploration down, 29 to go

--- SfigureB1.py
+++ SfigureB1.py
@@ -9,9 +9,10 @@ from params import Params
 from tqdm import tqdm
 
 ## A couple of helper functions to keep things tidy
+
 def mean_sem(a):
-    mean_a = np.mean(a)
-    sem_a = np.std(a) / np.sqrt(len(a))
+    mean_a = np.nanmean(a)
+    sem_a = np.nanstd(a) / np.sqrt(len(a))
     return mean_a,sem_a
 
 def plot_fill(xs,a,fig=None,ax=None,color='grey',alpha=0.5):
@@ -79,11 +80,13 @@ iterations = 1000
 params = Params()
 params.iterations = iterations
 
-print(params.awareness)
-print(params.outcome_params)
 params.size = 50
 
 assay_params = params.copy()
+
+print(params.awareness)
+print(params.outcome_params)
+
 #assay_params.baseline_effort = 0.535
 #assay_params.prior = True
 
@@ -99,7 +102,7 @@ f_sizes = []
 s_res = 10+1
 s_params = np.linspace(0,1.00,s_res)
 
-win_estimates = np.empty([s_res,2])
+win_estimates = np.empty([4,s_res,2])
 win_efforts = np.empty_like(win_estimates)
 win_outputs = np.empty_like(win_estimates)
 
@@ -107,80 +110,79 @@ loss_estimates = np.empty_like(win_estimates)
 loss_efforts = np.empty_like(win_estimates)
 loss_outputs = np.empty_like(win_estimates)
 
-for s_ in range(len(s_params)):
-    s = s_params[s_]
-    print(s)
-    params.outcome_params[0] = s
+def set_custom_params(params,name,value):
+    if name == 's':
+        params.outcome_params[0] = value
+    if name == 'l':
+        params.outcome_params[2] = value
+    if name == 'Sa':
+        params.awareness = value
+    if name == 'Sc':
+        params.acuity = value
     params.set_params()
+    return params
 
-    outcome_array,win_info_array,loss_info_array = run_sim(params)
-    """
-    for i in tqdm(range(iterations)):
-        focal_winner = Fish(i+2,params)
-        focal_loser = focal_winner.copy() 
+param_list = ['s','l','Sa','Sc']
 
-        f_sizes.append(focal_winner.size)
-## Stage a bunch of wins and losses against size-matched fish
+l_res = s_res
+a_res = s_res
 
-        staged_opp = Fish(0,params)
-        
-        staged_win = Fight(staged_opp,focal_winner,params,outcome=1)
-        staged_win.run_outcome()
-        focal_winner.update(True,staged_win)
+param_set = {'s':np.linspace(0,1,s_res),
+             'l':np.linspace(-1,1,l_res),
+             'Sa':np.linspace(0,1,a_res),
+             'Sc':np.linspace(0,1,a_res)
+}
 
-        staged_loss = Fight(staged_opp,focal_loser,params,outcome=0)
-        staged_loss.run_outcome()
-        focal_loser.update(False,staged_loss)
+default_params = [params.outcome_params[0],params.outcome_params[2],params.awareness,params.acuity]
 
-## Assay against size matched fish
-        assay_fish = Fish(1,assay_params)
+for p_ in range(len(param_list)):
+    params = Params()
+    params.iterations = iterations
+    params.size = 50
+    assay_params = params.copy()
 
-        assay_winner = Fight(assay_fish,focal_winner,params)
-        winner_output = assay_winner.run_outcome()
-        outcome_array[i,1] = winner_output
-
-        assay_loser = Fight(assay_fish,focal_loser,params)
-        loser_output = assay_loser.run_outcome()
-        outcome_array[i,0] = loser_output
-
-        win_info_array[i] = focal_winner.effort,focal_winner.estimate 
-        loss_info_array[i] = focal_loser.effort,focal_loser.estimate 
-
-        #if assay_fish.wager > focal_winner.wager:
-        #    w_probs.append(assay_winner.p_win)
-        #else:
-        #    w_probs.append(1-assay_winner.p_win)
-
-        #if assay_fish.wager > focal_loser.wager:
-        #    l_probs.append(assay_loser.p_win)
-        #else:
-        #    l_probs.append(1-assay_loser.p_win)
-    """
-
+    p = param_list[p_]
+    param_space = param_set[p]
+    for i_ in range(len(s_params)):
+        i = param_space[i_]
+        print(p,i)
+        params = set_custom_params(params,p,i)
+        print(params.outcome_params[0],params.L,params.A,params.C)
+        outcome_array,win_info_array,loss_info_array = run_sim(params)
 ## get win stats, using little helper function
-    win_outputs[s_] = mean_sem(outcome_array[:,1])
-    win_estimates[s_] = mean_sem(win_info_array[:,1])
-    win_efforts[s_] = mean_sem(win_info_array[:,0])
+        win_outputs[p_,i_] = mean_sem(outcome_array[:,1])
+        win_estimates[p_,i_] = mean_sem(win_info_array[:,1])
+        win_efforts[p_,i_] = mean_sem(win_info_array[:,0])
 
-    loss_outputs[s_] = mean_sem(outcome_array[:,0])
-    loss_estimates[s_] = mean_sem(loss_info_array[:,1])
-    loss_efforts[s_] = mean_sem(loss_info_array[:,0])
+        loss_outputs[p_,i_] = mean_sem(outcome_array[:,0])
+        loss_estimates[p_,i_] = mean_sem(loss_info_array[:,1])
+        loss_efforts[p_,i_] = mean_sem(loss_info_array[:,0])
 
-fig,axes = plt.subplots(3,1)
-plot_fill(s_params,win_estimates,ax=axes[0],color='gold')
-plot_fill(s_params,loss_estimates,ax=axes[0],color='darkblue')
+fig,axes = plt.subplots(3,4)
+for p_ in range(len(param_list)):
+    p = param_list[p_]
+    xs_params = param_set[p]
+    plot_fill(xs_params,win_estimates[p_],ax=axes[0,p_],color='gold')
+    plot_fill(xs_params,loss_estimates[p_],ax=axes[0,p_],color='darkblue')
 
-plot_fill(s_params,win_efforts,ax=axes[1],color='gold')
-plot_fill(s_params,loss_efforts,ax=axes[1],color='darkblue')
+    plot_fill(xs_params,win_efforts[p_],ax=axes[1,p_],color='gold')
+    plot_fill(xs_params,loss_efforts[p_],ax=axes[1,p_],color='darkblue')
 
-plot_fill(s_params,win_outputs,ax=axes[2],color='gold')
-plot_fill(s_params,loss_outputs,ax=axes[2],color='darkblue')
+    plot_fill(xs_params,win_outputs[p_],ax=axes[2,p_],color='gold')
+    plot_fill(xs_params,loss_outputs[p_],ax=axes[2,p_],color='darkblue')
 
-axes[2].set_xlabel('s value')
-axes[0].set_ylabel('Estimate')
-axes[1].set_ylabel('Assay effort')
-axes[2].set_ylabel('Assay win rate')
+axes[2,0].set_xlabel('s value')
+axes[2,1].set_xlabel('l value')
+axes[2,2].set_xlabel('Sigma_a value')
+axes[2,3].set_xlabel('Sigma_c value')
 
-for ax in axes:
-    ax.axvline(0.7,color='red',linestyle=':')
+axes[0,0].set_ylabel('Estimate')
+axes[1,0].set_ylabel('Assay effort')
+axes[2,0].set_ylabel('Assay win rate')
+
+for c_ in range(4):
+    for r_ in range(3):
+        ax = axes[r_,c_]
+        ax.axvline(default_params[c_],color='red',linestyle=':')
+
 plt.show()
