5028a777fcdf051c39d6ef786334bea8f414ce17
Ammon Perkes <perkes.ammon@gmail.com>
2023-09-13 10:51:49 -0700

    set up figures for SB2

--- SfigureB2.py
+++ SfigureB2.py
@@ -22,6 +22,10 @@ def tanthing(x):
     y = k*j + m 
     return y
 
+def ellie(x=0.7,a=2):
+    X = x**a
+    return X
+
 ## My original function
 def art(x=0.7,a=1):
     X = k * (m-x**a)
@@ -58,7 +62,8 @@ from scipy import stats
 def cory(x=0.7,s=50):
 ## Here, it's the CDF of being bigger, for some std
     x_abs = x*50 - 50
-    var = 1*x*50
+    #var = 1*x*50
+    var = s
     y = 1 - stats.norm.cdf(0,x_abs,var)
     return y
 
@@ -95,22 +100,57 @@ print('F more cleverness',timeit.Timer(frank).timeit(number=10000))
 print('logit-sigmoid:',timeit.Timer(perrin).timeit(number=10000))
 print('cdf',timeit.Timer(cory).timeit(number=10000))
 print('tanh',timeit.Timer(taco).timeit(number=10000))
+print('exp',timeit.Timer(ellie).timeit(number=10000))
 #funk = F._wager_curve_sig
-funk = perrin
+funk = cory
 #print(foo(0),foo(m),foo(1))
 print(funk(0.001),funk(m),funk(.99))
 print(hugh(0.001),hugh(0.5),hugh(.99))
 #xs = np.linspace(-3,3,100)
 xs = np.linspace(0.01,1,100)
 ys = funk(xs)
-if False:
-    for a in [0,0.25,0.5,1,2,4,np.inf]:
+
+fig,ax = plt.subplots()
+if True:
+    #for a in [0,0.25,0.5,1,2,4,np.inf]:
+    for a in [1,2,4,8,16]:
         F.params.a = a
         ys = funk(xs,a)
 
-        plt.plot(xs,ys,label='a= ' + str(a))
+        ax.plot(xs,ys,label='a= ' + str(a))
+    ax.set_xlabel('Relative wager')
+    ax.set_ylabel('Probability of underdog > favorite')
+    ax.legend(loc='upper left')
 
 #plt.legend()
-plt.plot(xs,ys)
+#plt.plot(xs,ys)
 #plt.axvline(m)
+
+fig2,ax2 = plt.subplots()
+
+xs = np.linspace(0,100.1,101)
+d1 = stats.norm.pdf(xs,50,8)
+d2 = stats.norm.pdf(xs,45,8)
+
+d3 = stats.norm.pdf(xs,50,4)
+d4 = stats.norm.pdf(xs,45,4)
+
+ax2.plot(xs,d1,alpha=0.5,color='tab:red')
+ax2.plot(xs,d2,color='tab:red',label='std=8')
+ax2.set_ylabel('Probability density')
+ax2.set_xlabel('Wager')
+ax2.legend()
+#ax2.plot(xs,d3,color='tab:blue',label='std=4')
+#ax2.plot(xs,d4,color='tab:blue')
+
+rel_xs = np.linspace(0,1,100)
+fig3,ax3 = plt.subplots()
+
+for l in [0,1/8,1/4,1/2,1,2,4,8]:
+    ax3.plot(rel_xs,ellie(rel_xs,l),label="l = " + str(l))
+
+ax3.set_xlabel('Relative wager')
+ax3.set_ylabel('Probability of upset')
+
+ax3.legend()
 plt.show()
