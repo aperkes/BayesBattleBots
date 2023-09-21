844cb7da52b2f5e64c1e301942d9233818ed3246
Ammon Perkes <perkes.ammon@gmail.com>
2023-09-21 16:14:06 -0700

    C2 works well now

--- SfigureC2.py
+++ SfigureC2.py
@@ -26,7 +26,7 @@ npcs = [FishNPC() for s in sizes]
 for n_ in range(len(npcs)):
     npcs[n_].size = sizes[n_]
 
-iterations = 3
+iterations = 1000
 #strategies = ['bayes','linear','no update']
 
 def mean_sem(a):
@@ -35,10 +35,13 @@ def mean_sem(a):
     return mean_a,sem_a
 
 def run_sim(params):
-    f_params = copy.deepcopy(params)
+    f_params = params
     outcome_array = np.empty([iterations,2,2]) ## iterations x w/l x sizes
     outcome_array.fill(np.nan)
-    platos_fish = Fish(0,f_params)
+    platos_winner = Fish(0,f_params)
+    platos_loser = Fish(0,f_params)
+    start_prior = np.array(platos_winner.prior)
+    start_estimate = platos_winner.estimate
     assay_opp = Fish(1,f_params) 
     for o_ in range(len(sizes)):
         opp = npcs[o_]
@@ -46,8 +49,16 @@ def run_sim(params):
 ## Stage a fight
             #focal_fish = Fish(0,f_params)
             #focal_loser = Fish(1,f_params)
-            focal_fish = copy.deepcopy(platos_fish)
-            focal_loser = copy.deepcopy(platos_fish)
+            #focal_winner = copy.deepcopy(platos_winner)
+            #focal_loser = copy.deepcopy(platos_loser)
+            #print(params.S,params.L,params.awareness,params.acuity)
+            focal_fish = platos_winner
+            focal_loser = platos_loser
+            focal_fish.estimate = start_estimate
+            focal_fish.prior = start_prior
+            focal_loser.estimate = start_estimate
+            focal_loser.prior = start_prior
+
             stage_fight = Fight(opp,focal_fish,f_params,outcome=1)
             stage_fight.run_outcome()
             focal_fish.update(True,stage_fight)
@@ -81,7 +92,9 @@ all_results = np.empty([s_res,l_res,a_res,c_res,2,2])
 def run_many_sims(s):
     many_results = np.empty([l_res,a_res,c_res,2,2])
     params = Params()
-    for l_ in range(l_res):
+    params.size = 50
+    params.prior = True
+    for l_ in tqdm(range(l_res)):
         l = l_list[l_]
         for a_ in range(a_res):
             a = a_list[a_]
@@ -93,11 +106,18 @@ def run_many_sims(s):
                 params.acuity = c    
                 params.set_params()
                 some_results = run_sim(params)
-                some_diff = np.abs(some_results[:,:,0] - some_results[:,:,1])
-                many_results[l_,a_,c_] = mean_sem(some_diff)
+                #if l_ == 1 and s_ == 7:
+                #   import pdb;pdb.set_trace()
+                #some_diff = some_results[:,:,0] - some_results[:,:,1]
+                mean_results,sem_results = mean_sem(some_results)
+                mean_diff = mean_results[:,1] - mean_results[:,0]
+                sem_diff = np.sum(sem_results,axis=1)
+                #many_results[l_,a_,c_] = mean_sem(some_diff)
+                many_results[l_,a_,c_,:,0] = mean_diff
+                many_results[l_,a_,c_,:,1] = sem_diff
     return many_results
 
-if True:
+if False:
     for s_ in tqdm(range(s_res)):
         s = s_list[s_]
         all_results[s_] = run_many_sims(s)
@@ -111,14 +131,38 @@ sem_winners = all_results[:,:,:,:,1,1]
 mean_losers = all_results[:,:,:,:,0,0]
 sem_losers = all_results[:,:,:,:,0,1]
 
-fig,axes = plt.subplots(2,2,sharey = 'row',sharex = 'col')
-vmin =0
-vmax = 1
-axes[0,0].imshow(mean_winners[:,:,5,1],vmin=vmin,vmax=vmax)
-axes[0,1].imshow(mean_winners[7,1,:,:],vmin=vmin,vmax=vmax)
-axes[1,0].imshow(mean_losers[:,:,5,1],vmin=vmin,vmax=vmax)
-axes[1,1].imshow(mean_losers[7,1,:,:],vmin=vmin,vmax=vmax)
+fig,axes = plt.subplots(2,2,sharey = 'col',sharex = 'col')
+vmin = -0.5
+vmax = 0.5
+cmap = 'RdBu_r'
+print('default params:',s_list[7],l_list[1],a_list[5],c_list[1])
 
+im = axes[0,0].imshow(mean_winners[:,:,5,1],vmin=vmin,vmax=vmax,cmap=cmap)
+axes[0,1].imshow(mean_winners[7,1,:,:],vmin=vmin,vmax=vmax,cmap=cmap)
+axes[1,0].imshow(mean_losers[:,:,5,1],vmin=vmin,vmax=vmax,cmap=cmap)
+axes[1,1].imshow(mean_losers[7,1,:,:],vmin=vmin,vmax=vmax,cmap=cmap)
+
+axes[1,0].set_xticks(range(l_res))
+axes[1,0].set_xticklabels(np.round(l_list,2),rotation=45)
+
+axes[0,0].set_yticks(range(s_res))
+axes[0,0].set_yticklabels(np.round(s_list,2))
+
+axes[0,0].set_ylabel('s value')
+axes[1,0].set_ylabel('s value')
+axes[1,0].set_xlabel('l value')
+
+axes[1,1].set_xticks(range(c_res))
+axes[1,1].set_xticklabels(np.round(c_list,2),rotation=45)
+
+axes[1,0].set_yticks(range(a_res))
+axes[1,0].set_yticklabels(np.round(a_list,2))
+
+axes[0,1].set_ylabel('a value')
+axes[1,1].set_ylabel('a value')
+axes[1,1].set_xlabel('c value')
+
+fig.colorbar(im,ax=axes)
 plt.show()
 
 ## Check against a fair fish
