af8cc7f833b27aff8c7b4ff3dfee23d6868a33e5
Ammon Perkes <perkes.ammon@gmail.com>
2023-09-28 11:25:13 -0700

    added a no-update line, moved some things around to make that work

--- figure5c_intensity.py
+++ figure5c_intensity.py
@@ -29,6 +29,8 @@ params.outcome_params = [0.5,-0.5,-0.8]
 #params.set_L()
 params.awareness = 0.5
 params.acuity = 0.1
+#params.update_method = 'Dont'
+
 
 params.set_params()
 #params.size=50
@@ -44,30 +46,48 @@ params.f_method = 'shuffled'
 
 params.n_fish = 5
 params.n_rounds = 30
-params.iterations = 500
+params.iterations = 1000
 #params.n_rounds = params.n_fights * (params.n_fish-1)
 
 ## Set up a tank
 cost_array = np.empty([params.iterations,params.n_fish,params.n_rounds * (params.n_fish-1)])
 error_array = np.empty([params.iterations,params.n_fish,params.n_rounds * (params.n_fish - 1) + 1])
 
+cost_array_null = np.empty_like(cost_array)
+error_array_null = np.empty_like(error_array)
+
+null_params = copy.deepcopy(params)
+null_params.update_method = None
+
 for i in tqdm(range(params.iterations)):
     fishes = [Fish(f,params) for f in range(5)]
-
     tank = Tank(fishes,params)
     tank.run_all(progress=False)
 
+    null_fishes = [Fish(f,null_params) for f in range(5)]
+    null_tank = Tank(null_fishes,null_params)
+    null_tank.run_all(progress=False)
+
     error_array[i] = sim._calc_error(tank)
+    error_array_null[i] = sim._calc_error(null_tank)
     for f in range(len(fishes)):
         cost_array[i,f] = np.array(tank.fishes[f].win_record)[:,3]
+        cost_array_null[i,f] = np.array(null_tank.fishes[f].win_record)[:,3]
     
 
 fig,(ax,ax1) = plt.subplots(1,2)
 
 mean_int = np.nanmean(cost_array,axis=(0,1))
 sem_int = np.nanstd(cost_array,axis=(0,1)) / np.sqrt(params.iterations)
-ax.plot(mean_int,color='black')
-ax.fill_between(range(len(mean_int)),mean_int-sem_int,mean_int+sem_int,color='gray',alpha=0.5)
+mean_int_null = np.nanmean(cost_array_null,axis=(0,1))
+sem_int_null = np.nanstd(cost_array_null,axis=(0,1)) / np.sqrt(params.iterations)
+
+ax.plot(mean_int,color='black',linestyle='dashed')
+ax.fill_between(range(len(mean_int)),mean_int-sem_int,mean_int+sem_int,color='royalblue',alpha=0.5)
+
+ax.plot(mean_int_null,color='black',linestyle='solid')
+ax.fill_between(range(len(mean_int_null)),mean_int_null-sem_int_null,mean_int_null+sem_int_null,color='gray',alpha=0.5)
+
 ax.set_xlabel('Contest number')
 ax.set_ylabel('Mean fight Intensity\n(+/- SEM of iterations')
 ax.set_title('Fight intensity decreases over repeated contests')
@@ -76,8 +96,16 @@ ax.set_title('Fight intensity decreases over repeated contests')
 ## Plot error
 mean_err = np.mean(error_array,axis=(0,1))
 sem_err = np.std(error_array,axis=(0,1)) / np.sqrt(params.iterations)
-ax1.plot(mean_err,color='black')
-ax1.fill_between(range(len(mean_err)),mean_err-sem_err,mean_err+sem_err,color='gray',alpha=0.5)
+
+mean_err_null = np.mean(error_array_null,axis=(0,1))
+sem_err_null = np.std(error_array_null,axis=(0,1)) / np.sqrt(params.iterations)
+
+ax1.plot(mean_err,color='black',linestyle='dashed')
+ax1.fill_between(range(len(mean_err)),mean_err-sem_err,mean_err+sem_err,color='royalblue',alpha=0.5)
+
+ax1.plot(mean_err_null,color='black')
+ax1.fill_between(range(len(mean_err_null)),mean_err_null-sem_err_null,mean_err_null+sem_err_null,color='gray',alpha=0.5)
+
 ax1.set_xlabel('Contest number')
 ax1.set_ylabel('Mean error \n(+/- SEM of iterations')
 ax1.set_title('Estimate error decreases over repeated contests')
