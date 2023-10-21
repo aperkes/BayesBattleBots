5d3022205f465db09c7fe7719603ba43490b70a8
Ammon Perkes <perkes.ammon@gmail.com>
2023-10-20 23:03:45 -0700

    set up for parallel

--- SfigureB12.py
+++ SfigureB12.py
@@ -1,16 +1,13 @@
 #from fish import Fish
-if False:
-    from OLDfish import Fish
-    from OLDtank import Tank
-    from OLDfight import Fight
-    from OLDparams import Params
-else:
-    from fish import Fish
-    from tank import Tank
-    from fight import Fight
-    from params import Params
+from fish import Fish
+from tank import Tank
+from fight import Fight
+from params import Params
 
 from matplotlib import pyplot as plt
+from joblib import Parallel, delayed
+import numpy as np
+from tqdm import tqdm
 
 params = Params()
 params.n_rounds = 30
@@ -25,11 +22,8 @@ fish0 = Fish(0,params)
 base_prior = fish0.prior
 params.prior = fish0.prior
 
-import numpy as np
 
-print(params.outcome_params)
 params.set_params()
-print(params.outcome_params)
 fishes = []
 sizes = [20,35,50,65,80]
 sizes = [50,50,50,50,50]
@@ -47,19 +41,18 @@ for f_ in range(5):
 params.size = 50
 
 iterations = 1000
-iterations = 3
+#iterations = 3
 s_res = 11
 s_list = np.linspace(0,1,s_res)
 
-results_array = np.empty([s_res,iterations,2])
 
-for s_ in range(s_res):
-    s = s_list[s_]
-    print(s)
+def run_one_sim(s,params):
+    params = params.copy()
+    some_results = np.empty([iterations,2])
     params.outcome_params[0] = s
     params.set_params()
 
-    for i in range(iterations):
+    for i in tqdm(range(iterations)):
         tank = Tank(fishes,params)
         tank.run_all(progress = False)
 
@@ -72,11 +65,30 @@ for s_ in range(s_res):
 
         initial_linearity,_ = tank._calc_linearity(init_idx)
         last_linearity,_ = tank._calc_linearity(final_idx)
-        results_array[s_,i] = initial_linearity,last_linearity
+        some_results[i] = initial_linearity,last_linearity
+    return some_results
 
-
+if False:
+    results_array = np.empty([s_res,iterations,2])
+    for s_ in range(s_res):
+        s = s_list[s_]
+        results_array[s_] = run_one_sim(s)
+else:
+    results_array = Parallel(n_jobs=11)(delayed(run_one_sim)(s,params) for s in s_list)
+    results_array = np.array(results_array)
 fig,ax = plt.subplots()
 
-ax.plot(np.mean(results_array[:,:,0],axis=1))
+mean_final = np.mean(results_array[:,:,0],axis=1)
+sem_final = np.std(results_array[:,:,0],axis=1) / np.sqrt(iterations)
+mean_init = np.mean(results_array[:,:,1],axis=1)
+
+
+ax.plot(s_list,mean_final,color='black')
+ax.plot(s_list,mean_init,color='black',linestyle=':')
+ax.fill_between(s_list,mean_final - sem_final,mean_final + sem_final,color='darkblue',alpha=0.5)
+ax.set_ylim([0.5,1])
+ax.set_ylabel('Linearity')
+ax.set_xlabel('s value')
+
 plt.show()
 
